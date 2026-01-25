823454053d233293a9f781312abffc9780fe1d07
Ahmedusman507 <ahmedusman50721@gmail.com>
2026-01-26 00:25:26 +0100

    logout functionality

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,13 +1,39 @@
-import React from 'react'
+"use client"
+import axios from "axios";
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
-const ProfilePage = () => {
+export default function ProfilePage() {
+
+  const router = useRouter()
+
+  const logout = async () => {
+
+    try {
+
+      await axios.get('/api/users/logout')
+      toast.success('Logout successful')
+      router.push('/login')
+      
+    } catch (error: any) {
+
+      console.log(error.message);
+      toast.error(error.message);
+    }
+
+  }
+
   return (
     <div className='flex flex-col items-center justify-center min-h-screen py-2'>
       <h1>Profile</h1>
-      <br />
+      <hr />
       <p>Profile page</p>
+      <hr />
+      <button
+      onClick={logout}
+      className='bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Logout</button>
     </div>
   )
 }
 
-export default ProfilePage
+
