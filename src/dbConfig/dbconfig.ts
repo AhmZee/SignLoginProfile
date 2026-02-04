54c499ffa5972f4b29226a10cb55b8c14bc542c5
Ahmedusman507 <ahmedusman50721@gmail.com>
2026-02-04 08:58:52 +0100

    code formated

--- src/dbConfig/dbconfig.ts
+++ src/dbConfig/dbconfig.ts
@@ -1,20 +1,21 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export async function connect() {
-    try {
-        mongoose.connect(process.env.MONGO_URL!);
-        const connection = mongoose.connection;
+  try {
+    mongoose.connect(process.env.MONGO_URL!);
+    const connection = mongoose.connection;
 
-        connection.on('connected', () => {
-            console.log('MongoDB connected successfully');
-        })
+    connection.on("connected", () => {
+      console.log("MongoDB connected successfully");
+    });
 
-        connection.on('error', (err) => {
-            console.log('MongoDB connection error. please make sure MongoDB is runing.' + err);
-        })
-
-    } catch (error) {
-        console.log('Something goes wrong!');
-        console.log(error);
-    }
+    connection.on("error", (err) => {
+      console.log(
+        "MongoDB connection error. please make sure MongoDB is runing." + err,
+      );
+    });
+  } catch (error) {
+    console.log("Something goes wrong!");
+    console.log(error);
+  }
 }
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,32 +1,26 @@
-import { NextRequest, NextResponse } from 'next/server'
-
+import { NextRequest, NextResponse } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function proxy(request: NextRequest) {
-    const path = request.nextUrl.pathname
+  const path = request.nextUrl.pathname;
 
-  const isPublicPath = path === '/login' || path === '/signup' || path === '/verify-email'
+  const isPublicPath =
+    path === "/login" || path === "/signup" || path === "/verify-email";
 
-  const token = request.cookies.get('token')?.value || ''
+  const token = request.cookies.get("token")?.value || "";
 
-  if(isPublicPath && token) {
-    return NextResponse.redirect(new URL('/', request.url))
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.url));
   }
-  
-  if(!isPublicPath && !token) {
-        return NextResponse.redirect(new URL('login', request.url))
+
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("login", request.url));
   }
 }
- 
+
 // Alternatively, you can use a default export:
 // export default function proxy(request: NextRequest) { ... }
- 
+
 export const config = {
-  matcher: [
-    '/',
-    '/profile',
-    '/login',
-    '/signup',
-    '/verify-email',
-  ],
-}
+  matcher: ["/", "/profile", "/login", "/signup", "/verify-email"],
+};
